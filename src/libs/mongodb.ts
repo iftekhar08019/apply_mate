6e7239acad5ee1935abc6a9d5dfda338660ba1b3
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-10-02 23:20:46 +0600

    added mongodb.ts file

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,5 +1,19 @@
+import { MongoClient, Db } from "mongodb";
+
+let client: MongoClient;
+let db: Db;
 
 export async function connectToDatabase() {
+  if (db) return { client, db };
 
-    
+  client = new MongoClient(process.env.NEXT_PUBLIC_MONGODB_URI!);
+  await client.connect();
+  db = client.db(process.env.NEXT_PUBLIC_DB_NAME!);
+
+  return { client, db };
 }
+
+export const collectionName = {
+  USERS: "users",
+  APPLICATIONS: "applications",
+};
